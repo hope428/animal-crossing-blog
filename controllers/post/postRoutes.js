37dad5a34674d8ebaf89277c467dfa8257b8c8b1
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-27 14:43:22 -0500

    added 404 page. added view for post by id

--- controllers/post/postRoutes.js
+++ controllers/post/postRoutes.js
@@ -1,7 +1,25 @@
+const { Post, User } = require('../../models');
+
 const router = require('express').Router();
 
 router.get('/:id', async(req, res) => {
-    res.send("you're at post " + req.params.id)
+
+    try {
+        const thisPost = await Post.findByPk(req.params.id, {
+            include: {model: User}
+        })
+
+        if(!thisPost){
+            res.render('404')
+        }
+
+
+        const thisPostData = thisPost.get({plain: true})
+        
+        res.render('post', {post: thisPostData})
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 module.exports = router;
